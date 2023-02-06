36b6cc2f2d45794fd131368cd47893ecab4e3668
anya.shan <anya.shanaida@yahoo.com>
2023-02-06 17:52:08 +0200

    styled UserPage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,12 +26,13 @@ export const App = () => {
           path="login"
           element={<RestrictedRoute component={<LoginPage />} redirectTo="/" />}
         />
-        <Route
+        {/* <Route
           path="user"
           element={
             <PrivateRoute component={<UserPage />} redirectTo="/login" />
           }
-        />
+        /> */}
+        <Route path="user" element={<UserPage />} />
         <Route
           path="news"
           element={<PrivateRoute component={<NewsPage />} redirectTo="/news" />}
--- src/pages/UserPage/UserPage.styled.js
+++ src/pages/UserPage/UserPage.styled.js
@@ -1,13 +1,25 @@
 import styled from 'styled-components';
 
 const MainContainer = styled.div`
-  padding: 0 20px 80px 20px;
+  outline: 1px solid green;
+
+  padding: 60px 0 80px 0px;
   display: flex;
   flex-direction: column;
-  gap: 47px;
+  gap: 40px;
 
-  background-color: ${props => props.theme.colors.commonBackground};
-  font-family: 'Manrope';
+  font-family: ${props => props.theme.fonts[0]};
+
+  @media screen and (min-width: ${props => props.theme.breakpoints.tablet}) {
+    padding: 88px 0 100px 0px;
+    gap: 20px;
+  }
+
+  @media screen and (min-width: ${props => props.theme.breakpoints.desktop}) {
+    padding: 58px 0 40px 0px;
+    flex-direction: row;
+    gap: 32px;
+  }
 `;
 
 const ContainerWrapp = styled.div`
@@ -16,20 +28,46 @@ const ContainerWrapp = styled.div`
 
 const Title = styled.h2`
   margin-bottom: 18px;
-  font-family: 'Manrope';
-  font-weight: 500;
-  font-size: 20px;
+
+  font-weight: ${props => props.theme.fontWeights.medium};
+  font-size: ${props => props.theme.fontSizes.text.xl};
   line-height: 1.35;
   letter-spacing: 0.04em;
+
+  @media screen and (min-width: ${props => props.theme.breakpoints.tablet}) {
+    margin-bottom: 40px;
+    font-size: ${props => props.theme.fontSizes.headers.s};
+    line-height: 1.357;
+  }
+
+  @media screen and (min-width: ${props => props.theme.breakpoints.desktop}) {
+    margin-bottom: 24px;
+  }
 `;
 
 const UserContainer = styled.div`
-  padding: 20px 16px;
+  outline: 1px solid green;
+
+  padding: 20px 12px 20px 16px;
   display: flex;
   flex-direction: column;
+  gap: 42px;
   border-radius: 20px;
   background-color: ${props => props.theme.colors.white};
   box-shadow: ${props => props.theme.shadows.cardsShadowUser};
+
+  @media screen and (min-width: ${props => props.theme.breakpoints.tablet}) {
+    padding: 20px 16px 18px 16px;
+    margin-left: -32px;
+    gap: 0;
+    border-radius: 0px 40px 40px 0px;
+  }
+
+  @media screen and (min-width: ${props => props.theme.breakpoints.desktop}) {
+    padding: 20px 40px 24px 32px;
+    margin-left: -16px;
+    gap: 24px;
+  }
 `;
 
 const styles = { MainContainer, ContainerWrapp, Title, UserContainer };
