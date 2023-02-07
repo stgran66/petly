9b335cac41b43986ca060dbb8f9c11d9c41c9f05
anya.shan <anya.shanaida@yahoo.com>
2023-02-07 16:33:54 +0200

    remove hooks

--- src/components/User/Logout/Logout.jsx
+++ src/components/User/Logout/Logout.jsx
@@ -1,16 +1,12 @@
 import { useDispatch } from 'react-redux';
 import operations from 'redux/auth/operations';
-import hooks from 'hooks';
 import styles from './Logout.styled';
 const { LogoutButton, LogoutIcon } = styles;
 
 const { logOut } = operations;
-const { useAuth } = hooks;
 
-console.log(useAuth);
 const Logout = () => {
   const dispatch = useDispatch();
-  // const { user } = useAuth();
 
   const handleLogOut = () => dispatch(logOut());
   return (
