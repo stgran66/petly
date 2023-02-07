0e8fca43e7126a05d4df86155cd55e60478b7736
Volodymyr Velch <velch83@gmail.com>
2023-02-07 23:05:32 +0200

    modalChanges

--- src/components/notices/NoticeModal/NoticeModal.styled.jsx
+++ src/components/notices/NoticeModal/NoticeModal.styled.jsx
@@ -7,10 +7,24 @@ const Container = styled.div`
   padding: 60px 20px 40px;
   background-color: ${props => props.theme.colors.white};
   border-radius: 20px;
+  @media screen and (min-width: ${props => props.theme.breakpoints.tablet}) {
+    max-width: 704px;
+    padding: 32px 20px;
+  }
 `;
 const Wrapper = styled.div`
   & + p {
     margin-bottom: 40px;
+  }
+  @media screen and (min-width: ${props => props.theme.breakpoints.tablet}) {
+    display: grid;
+    grid-template-columns: 288px 356px;
+    gap: 20px;
+    margin-bottom: 28px;
+
+    & + p {
+      margin-bottom: 32px;
+    }
   }
 `;
 
@@ -21,6 +35,13 @@ const ImgWrapper = styled.div`
 
   border-radius: 0px 0px 40px 40px;
   overflow: hidden;
+  @media screen and (max-width: ${props => props.theme.breakpoints.tablet}) {
+    margin: 0 auto 16px auto;
+  }
+  @media screen and (min-width: ${props => props.theme.breakpoints.tablet}) {
+    width: 288px;
+    height: 328px;
+  }
 `;
 const Img = styled.img`
   object-fit: cover;
@@ -45,6 +66,10 @@ const Category = styled.span`
   border-bottom-right-radius: 20px;
 `;
 
+export const ListWrapper = styled.div`
+  width: 100%;
+`;
+
 const Title = styled.h2`
   margin-bottom: 16px;
   font-weight: 700;
@@ -52,10 +77,20 @@ const Title = styled.h2`
   line-height: 1.375;
   letter-spacing: -0.01em;
   word-wrap: break-word;
+  @media screen and (min-width: ${props => props.theme.breakpoints.tablet}) {
+    width: 90%;
+    margin-bottom: 20px;
+
+    font-size: 28px;
+    line-height: 1.35;
+  }
 `;
 
 const List = styled.ul`
   margin-bottom: 28px;
+  @media screen and (min-width: ${props => props.theme.breakpoints.tablet}) {
+    margin-bottom: 0;
+  }
 `;
 
 const Item = styled.li`
@@ -71,6 +106,90 @@ const ItemInfo = styled.p`
   font-size: 14px;
   line-height: 1.35;
   color: ${props => props.theme.colors.Text};
+  @media screen and (min-width: ${props => props.theme.breakpoints.tablet}) {
+    min-width: 120px;
+
+    font-size: 16px;
+    line-height: 1.5;
+  }
+`;
+
+const BtnWrapper = styled.div`
+  @media screen and (min-width: ${props => props.theme.breakpoints.tablet}) {
+    display: flex;
+    flex-direction: row-reverse;
+    padding-right: 20px;
+  }
+`;
+
+const ContactButton = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 40px;
+  margin-bottom: 12px;
+
+  font-size: 16px;
+  line-height: 1.375;
+  letter-spacing: 0.04em;
+  color: ${props => props.theme.colors.white};
+  text-align: center;
+
+  background-color: ${props => props.theme.colors.accent};
+  border-radius: 40px;
+  transition: background-color ${props => props.theme.tranistions.normal};
+
+  @media screen and (min-width: ${props => props.theme.breakpoints.tablet}) {
+    width: 160px;
+    margin-bottom: 0;
+  }
+
+  &:hover,
+  &:focus {
+    background-color: #ff6101;
+  }
+`;
+
+const ChangeFavoriteStatusBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 40px;
+
+  font-size: 16px;
+  line-height: 1.375;
+  letter-spacing: 0.04em;
+  color: ${props => props.theme.colors.text};
+
+  border: 2px solid ${props => props.theme.colors.accent};
+  border-radius: 40px;
+  cursor: pointer;
+  transition: border-color ${props => props.theme.tranistions.long};
+
+  & > svg {
+    width: 16px;
+    height: 16px;
+    margin-left: 8px;
+
+    fill: ${props => props.theme.colors.accent};
+    transition: fill ${props => props.theme.tranistions.long};
+  }
+
+  @media screen and (min-width: ${props => props.theme.breakpoints.tablet}) {
+    width: 160px;
+    margin-right: 12px;
+  }
+
+  &:hover,
+  &:focus {
+    border-color: #ff6101;
+
+    & > svg {
+      fill: #ff6101;
+    }
+  }
 `;
 
 const style = {
@@ -79,10 +198,14 @@ const style = {
   ImgWrapper,
   Img,
   Category,
+  ListWrapper,
   Title,
   List,
   Item,
   ItemInfo,
+  BtnWrapper,
+  ContactButton,
+  ChangeFavoriteStatusBtn,
 };
 
 export default style;
