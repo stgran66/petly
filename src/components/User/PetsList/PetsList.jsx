b35820ffff5f01bdf73301149730859392617aae
anya.shan <anya.shanaida@yahoo.com>
2023-02-11 19:12:33 +0200

    fix user

--- src/components/User/PetsList/PetsList.jsx
+++ src/components/User/PetsList/PetsList.jsx
@@ -1,25 +1,37 @@
+// import React, { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import userSelectors from 'redux/user/selectors';
+
 import styles from './PetsList.styled';
 import PetItem from './PetItem';
 
 const { List, Item } = styles;
+// const { selectUserInfo, selectLoadingUser, selectErrorUser } = userSelectors;
 
 const PetsList = () => {
+  // const user = useSelector(selectUserInfo);
+  // const isLoading = useSelector(selectLoadingUser);
+  // const error = useSelector(selectErrorUser);
   return (
-    <List>
-      <Item>
-        <PetItem />
-      </Item>
-      <Item>
-        <PetItem />
-      </Item>
-      <Item>
-        <PetItem />
-      </Item>
-      <Item>
-        <PetItem />
-      </Item>
-    </List>
+    <>
+      {/* {error && <h2>You don't have any зуеі yet </h2>} */}
+      <List>
+        <Item>
+          <PetItem />
+        </Item>
+        <Item>
+          <PetItem />
+        </Item>
+        <Item>
+          <PetItem />
+        </Item>
+        <Item>
+          <PetItem />
+        </Item>
+      </List>
+    </>
   );
+
   // return pets.map(pet => (
   //   <li key={pet.id}>
   //     <PetsItem item={pet} />
