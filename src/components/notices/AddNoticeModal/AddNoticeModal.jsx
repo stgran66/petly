daaf1bdc0da0ad631bc3974ba9b85225c290b431
IvannaKhomik <ivanna.khomik@gmail.com>
2023-02-11 16:19:07 +0200

    merge main

--- src/components/notices/AddNoticeModal/AddNoticeModal.jsx
+++ src/components/notices/AddNoticeModal/AddNoticeModal.jsx
@@ -55,15 +55,16 @@ const AddNoticeModal = ({ setIsModalOpen }) => {
     setIsModalOpen(false);
   };
 
-  const onHandleSubmit = async values => {
+  const onHandleSubmit = async (values, actions) => {
     const notice = await {
       ...values,
       category: categoryValue,
       sex: sexValue,
     };
     console.log(notice);
+    actions.setSubmitting(false);
     setIsModalOpen(false);
-    dispatch(addNotice(notice));
+    // dispatch(addNotice(notice));
   };
 
   return (
@@ -80,140 +81,138 @@ const AddNoticeModal = ({ setIsModalOpen }) => {
           <IconClose />
         </InputButton>
         <Formik initialValues={initialValues} onSubmit={onHandleSubmit}>
-          <Form>
-            {firstPage ? (
-              <>
-                <ModalCategoryGroup
-                  onChange={handleChangeRadio}
-                  name="category"
-                >
-                  <ModalCategoryLabel
-                    value="lost/found"
-                    control={<ModalCategoryField />}
-                    label="lost/found"
-                    checked={categoryValue === 'lost/found' && true}
-                  />
-                  <ModalCategoryLabel
-                    value="in good hands"
-                    control={<ModalCategoryField />}
-                    label="in good hands"
-                    checked={categoryValue === 'in good hands' && true}
-                  />
-                  <ModalCategoryLabel
-                    value="sell"
-                    checked={categoryValue === 'sell' && true}
-                    control={<ModalCategoryField />}
-                    label="sell"
-                  />
-                </ModalCategoryGroup>
-                <ModalFieldLabel htmlFor="title">
-                  Title of ad
-                  <ModalField
-                    id="title"
-                    name="title"
-                    placeholder="Type pet info"
-                  />
-                </ModalFieldLabel>
-
-                <ModalFieldLabel htmlFor="name">
-                  Name pet
-                  <ModalField
-                    id="name"
-                    name="name"
-                    placeholder="Type name pet"
-                  />
-                </ModalFieldLabel>
-
-                <ModalFieldLabel htmlFor="birthday">
-                  Date of birth
-                  <ModalField
-                    id="birthday"
-                    name="birthday"
-                    placeholder="Type date of birth"
-                  />
-                </ModalFieldLabel>
-
-                <ModalFieldLabel htmlFor="breed">
-                  Type breed
-                  <ModalField
-                    id="breed"
-                    name="breed"
-                    placeholder="Type breed"
-                  />
-                </ModalFieldLabel>
-                <ModalBtnWrap>
-                  <ModalBtn
-                    type="button"
-                    active
-                    onClick={() => setFirstPage(false)}
+          {props => (
+            <Form>
+              {firstPage ? (
+                <>
+                  <ModalCategoryGroup
+                    onChange={handleChangeRadio}
+                    name="category"
                   >
-                    Next
-                  </ModalBtn>
-                  <ModalBtn type="button" onClick={() => handleCancel()}>
-                    Cancel
-                  </ModalBtn>
-                </ModalBtnWrap>
-              </>
-            ) : (
-              <>
-                <ModalRadioGroup
-                  value="{sexValue}"
-                  onChange={handleChangeRadio}
-                  name="sex"
-                >
-                  <ModalLabel
-                    value="male"
-                    control={<ModalSexField />}
-                    label="male"
-                    checked={sexValue === 'male' && true}
-                  />
+                    <ModalCategoryLabel
+                      value="lost/found"
+                      control={<ModalCategoryField />}
+                      label="lost/found"
+                      checked={categoryValue === 'lost/found' && true}
+                    />
+                    <ModalCategoryLabel
+                      value="in good hands"
+                      control={<ModalCategoryField />}
+                      label="in good hands"
+                      checked={categoryValue === 'in good hands' && true}
+                    />
+                    <ModalCategoryLabel
+                      value="sell"
+                      checked={categoryValue === 'sell' && true}
+                      control={<ModalCategoryField />}
+                      label="sell"
+                    />
+                  </ModalCategoryGroup>
+                  <ModalFieldLabel>
+                    Title of ad
+                    <ModalField name="title" placeholder="Type pet info" />
+                  </ModalFieldLabel>
 
-                  <ModalLabel
-                    value="female"
-                    control={<ModalSexField />}
-                    label="female"
-                    checked={sexValue === 'female' && true}
-                  />
-                </ModalRadioGroup>
-                <ModalFieldLabel htmlFor="place">
-                  Location
-                  <ModalField
-                    id="place"
-                    name="place"
-                    placeholder="Type pet location"
-                  />
-                </ModalFieldLabel>
-                <ModalFieldLabel htmlFor="price">
-                  Price
-                  <ModalField
-                    id="price"
-                    name="price"
-                    placeholder="Type pet price"
-                  />
-                </ModalFieldLabel>
-                <ModalFieldLabel htmlFor="imageUrl">
-                  Load the pet's image
-                  <ModalField id="imageUrl" name="imageUrl" type="file" />
-                </ModalFieldLabel>
-                <ModalFieldLabel htmlFor="comments">
-                  Comments
-                  <ModalField
-                    id="comments"
-                    name="comments"
-                    placeholder="Type your comments"
-                  />
-                </ModalFieldLabel>
-                <ModalBtnWrap>
-                  <ModalBtn type="button" onClick={() => setFirstPage(true)}>
-                    Back
-                  </ModalBtn>
-                  <ModalBtn type="submit" active>
-                    Done
-                  </ModalBtn>
-                </ModalBtnWrap>
-              </>
-            )}
-          </Form>
+                  <ModalFieldLabel htmlFor="name">
+                    Name pet
+                    <ModalField
+                      id="name"
+                      name="name"
+                      placeholder="Type name pet"
+                    />
+                  </ModalFieldLabel>
+
+                  <ModalFieldLabel htmlFor="birthday">
+                    Date of birth
+                    <ModalField
+                      id="birthday"
+                      name="birthday"
+                      placeholder="Type date of birth"
+                    />
+                  </ModalFieldLabel>
+
+                  <ModalFieldLabel htmlFor="breed">
+                    Type breed
+                    <ModalField
+                      id="breed"
+                      name="breed"
+                      placeholder="Type breed"
+                    />
+                  </ModalFieldLabel>
+                  <ModalBtnWrap>
+                    <ModalBtn
+                      type="button"
+                      active
+                      onClick={() => setFirstPage(false)}
+                    >
+                      Next
+                    </ModalBtn>
+                    <ModalBtn type="button" onClick={() => handleCancel()}>
+                      Cancel
+                    </ModalBtn>
+                  </ModalBtnWrap>
+                </>
+              ) : (
+                <>
+                  <ModalRadioGroup
+                    value="{sexValue}"
+                    onChange={handleChangeRadio}
+                    name="sex"
+                  >
+                    <ModalLabel
+                      value="male"
+                      control={<ModalSexField />}
+                      label="male"
+                      checked={sexValue === 'male' && true}
+                    />
+
+                    <ModalLabel
+                      value="female"
+                      control={<ModalSexField />}
+                      label="female"
+                      checked={sexValue === 'female' && true}
+                    />
+                  </ModalRadioGroup>
+                  <ModalFieldLabel htmlFor="place">
+                    Location
+                    <ModalField
+                      id="place"
+                      name="place"
+                      placeholder="Type pet location"
+                    />
+                  </ModalFieldLabel>
+                  <ModalFieldLabel htmlFor="price">
+                    Price
+                    <ModalField
+                      id="price"
+                      name="price"
+                      placeholder="Type pet price"
+                    />
+                  </ModalFieldLabel>
+                  <ModalFieldLabel htmlFor="imageUrl">
+                    Load the pet's image
+                    <ModalField id="imageUrl" name="imageUrl" type="file" />
+                  </ModalFieldLabel>
+                  <ModalFieldLabel htmlFor="comments">
+                    Comments
+                    <ModalField
+                      id="comments"
+                      name="comments"
+                      placeholder="Type your comments"
+                    />
+                  </ModalFieldLabel>
+                  <ModalBtnWrap>
+                    <ModalBtn type="button" onClick={() => setFirstPage(true)}>
+                      Back
+                    </ModalBtn>
+                    <ModalBtn type="submit" active>
+                      Done
+                    </ModalBtn>
+                  </ModalBtnWrap>
+                </>
+              )}
+            </Form>
+          )}
         </Formik>
       </Modal>
     </ModalBackdrop>
