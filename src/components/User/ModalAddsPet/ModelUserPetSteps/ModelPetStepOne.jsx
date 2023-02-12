9d95830eacd51ee4b98ce919db923a704ecd23a8
anya.shan <anya.shanaida@yahoo.com>
2023-02-12 18:23:09 +0200

    add validation

--- src/components/User/ModalAddsPet/ModelUserPetSteps/ModelPetStepOne.jsx
+++ src/components/User/ModalAddsPet/ModelUserPetSteps/ModelPetStepOne.jsx
@@ -1,4 +1,33 @@
-import { Formik, Form, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+
+const RegExp = /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/;
+const birthdayRegExp =
+  /^([0-2][0-9]|(3)[0-1])\.(((0)[0-9])|((1)[0-2]))\.\d{4}$/;
+
+let schema = yup.object().shape({
+  name: yup
+    .string()
+    .min(2)
+    .max(16)
+    .matches(
+      RegExp,
+      "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+    )
+    .required(),
+  birthday: yup
+    .string()
+    .matches(birthdayRegExp, 'birthday should be in dd.mm.yyyy format'),
+  breed: yup
+    .string()
+    .min(2)
+    .max(16)
+    .matches(
+      RegExp,
+      'Breed may contain only letters, apostrophe, dash and spaces.'
+    )
+    .required(),
+});
 
 const ModelPetStepOne = ({ data }) => {
   const handleFormSubmit = (values, { resetForm }) => {
@@ -16,35 +45,37 @@ const ModelPetStepOne = ({ data }) => {
       validationSchema={schema}
       onSubmit={handleFormSubmit}
     >
-      <FormWrap>
+      <Form>
         <div>
-          <label htmlFor={nameInputId}>Name</label>
-          <FieldWrap
+          <label htmlFor="name">Name pet</label>
+          <Field
             type="text"
             name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
-            placeholder="Rosie Simpson"
-            id={nameInputId}
+            placeholder="Type name pet"
+            id="name"
           />
           <ErrorMessage component="span" name="name" />
-          <label htmlFor={numberInputId}>Number</label>
-          <FieldWrap
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+
+          <label htmlFor="birthday">Date of birth</label>
+          <Field
+            type="text"
+            name="birthday"
             required
-            placeholder="459-12-56"
-            id={numberInputId}
+            placeholder="Type date of birth"
+            id="birthday"
           />
-          <ErrorMessage component="span" name="number" />
+          <ErrorMessage component="span" name="birthday" />
+
+          <label htmlFor="birthday">Breed</label>
+          <Field type="text" name="breed" placeholder="Type breed" id="breed" />
+          <ErrorMessage component="span" name="breed" />
         </div>
-        <button type="submit" disabled={false}>
-          Add contact
-        </button>
-      </FormWrap>
+        <div>
+          <button type="submit">Next</button>
+          <button type="submit">Camcel</button>
+        </div>
+      </Form>
     </Formik>
     // <>
     //   <h2>Hello One!</h2>
