b34a1885af3f7f16ca74af644aa77a4b40186885
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2023-02-14 12:43:50 +0100

    fix bugs

--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -1,3 +1,4 @@
+import { useLocation } from 'react-router-dom';
 import styles from './Nav.styled';
 
 const navItems = [
@@ -8,11 +9,18 @@ const navItems = [
 
 const Nav = ({ closeBurgerMenu }) => {
   const { NavList, Link } = styles;
+  const location = useLocation();
   return (
     <NavList>
       {navItems.map(({ href, text, end }) => (
         <li key={href}>
-          <Link to={href} title={text} end={end} onClick={closeBurgerMenu}>
+          <Link
+            to={href}
+            title={text}
+            end={end}
+            onClick={closeBurgerMenu}
+            state={{ from: location }}
+          >
             {text}
           </Link>
         </li>
