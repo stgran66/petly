3e3abca801e65ba5e99160b259cde5e36322edd5
Volodymyr Velch <velch83@gmail.com>
2023-02-14 22:39:20 +0200

    fieldNameFix

--- src/hooks/useCategories.js
+++ src/hooks/useCategories.js
@@ -3,14 +3,14 @@ import { useEffect } from 'react';
 const useCategories = (category, func) => {
   useEffect(() => {
     switch (category) {
-      case 'sell':
-        func('Sell');
-        break;
       case 'lost-found':
         func('Lost/found');
         break;
       case 'for-free':
         func('In good hands');
+        break;
+      case 'sell':
+        func('Sell');
         break;
       default:
         func('lost-found');
