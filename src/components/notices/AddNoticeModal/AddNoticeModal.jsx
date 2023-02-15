304af9b8a0c8fd4bf7b13bfbfb09f10dbde2bc6d
IvannaKhomik <ivanna.khomik@gmail.com>
2023-02-15 10:16:51 +0200

    fix addNotices styles

--- src/components/notices/AddNoticeModal/AddNoticeModal.jsx
+++ src/components/notices/AddNoticeModal/AddNoticeModal.jsx
@@ -6,10 +6,11 @@ import styles from './AddNoticeModal.styled';
 import FemaleIcon from '../../../images/female-icon.svg';
 import plusIcon from '../../../images/plus-icon.svg';
 import MaleIcon from '../../../images/male-icon.svg';
-import * as Yup from 'yup';
-import { parse, isDate } from 'date-fns';
+import validationSchemas from './ValidationModal';
 import { Box } from '@mui/system';
 
+const { schema, sellPetSchema } = validationSchemas;
+const { addNotice } = operations;
 const {
   ModalBackdrop,
   ModalTextarea,
@@ -38,7 +39,7 @@ const {
   RadiImg,
   PetImg,
 } = styles;
-const { addNotice } = operations;
+
 const initialValues = {
   category: 'sell',
   title: '',
@@ -52,82 +53,6 @@ const initialValues = {
   price: '',
 };
 
-const sellPetSchema = Yup.object().shape({
-  title: Yup.string()
-    .min(2, 'Title should be from 2 to 48 symbols')
-    .max(48, 'Title should be from 2 to 48 symbols')
-    .matches(
-      /^[a-zA-zа-яіїєА-ЯІЇЄ,.! ]+$/,
-      'Title should be from 2 to 48 symbols'
-    )
-    .required('The title is required'),
-  name: Yup.string()
-    .min(2, 'Name should be from 2 to 16 symbols')
-    .max(16, 'Name should be from 2 to 16 symbols')
-    .required('The name is required'),
-  birthday: Yup.date()
-    .transform((value, originalValue) => {
-      const parsedDate = isDate(originalValue)
-        ? originalValue
-        : parse(originalValue, 'dd.MM.yyyy', new Date());
-      return parsedDate;
-    })
-    .min(new Date(1950, 1, 1), 'birthday should be after 1950')
-    .max(new Date(), 'birthday could not be after today')
-    .typeError('date should be in dd.mm.yyyy format')
-    .required('Date of birth is required'),
-  breed: Yup.string()
-    .min(2, 'Breed should be from 2 to 16 symbols')
-    .max(16, 'Breed should be from 2 to 16 symbols')
-    .required('The breed is required'),
-  sex: Yup.string(),
-  place: Yup.string().min(4, 'Too Short!').max(60, 'Too Long!').required(),
-  price: Yup.string().required('The price is required'),
-  comments: Yup.string()
-    .min(8, 'Too Short!')
-    .max(120, 'Too Long!')
-    .required('The comments are required'),
-  imageUrl: Yup.mixed().required('Image is required (jpg, jpeg, png)'),
-});
-
-const schema = Yup.object().shape({
-  title: Yup.string()
-    .min(2, 'Title should be from 2 to 48 symbols')
-    .max(48, 'Title should be from 2 to 48 symbols')
-    .matches(
-      /^[a-zA-zа-яіїєА-ЯІЇЄ,.! ]+$/,
-      'title should be from 2 to 48 symbols'
-    )
-    .required('The title is required'),
-  name: Yup.string()
-    .min(2, 'Name should be from 2 to 16 symbols')
-    .max(16, 'Name should be from 2 to 16 symbols'),
-  birthday: Yup.date()
-    .transform((value, originalValue) => {
-      const parsedDate = isDate(originalValue)
-        ? originalValue
-        : parse(originalValue, 'dd.MM.yyyy', new Date());
-      return parsedDate;
-    })
-    .min(new Date(1950, 1, 1), 'birthday should be after 1950')
-    .max(new Date(), 'birthday could not be after today')
-    .typeError('date should be in dd.mm.yyyy format'),
-  breed: Yup.string()
-    .min(2, 'Breed should be from 2 to 16 symbols')
-    .max(16, 'Breed should be from 2 to 16 symbols'),
-  sex: Yup.string().required(),
-  place: Yup.string().min(4, 'Too Short!').max(60, 'Too Long!').required(),
-  price: Yup.string()
-    .min(1, 'Too Short!')
-    .matches(/^[1-9][0-9]*$/, 'price cannot starts from zero'),
-  comments: Yup.string()
-    .min(8, 'Comments should be from 8 to 120 symbols')
-    .max(120, 'Comments should be from 8 to 120 symbols')
-    .required('The comments are required'),
-  imageUrl: Yup.mixed().required('Image is required (jpg, jpeg, png)'),
-  category: Yup.string().required(),
-});
-
 const AddNoticeModal = ({ isModalOpen, setIsModalOpen }) => {
   const [selectedCategoryValue, setSelectedCategoryValue] = useState('sell');
   const [selectedSexValue, setSelectedSexValue] = useState('');
@@ -137,6 +62,7 @@ const AddNoticeModal = ({ isModalOpen, setIsModalOpen }) => {
   const dispatch = useDispatch();
   const [formData, setFormData] = useState(initialValues);
   const [isAddImg, setIsAddImg] = useState(initialValues.imageUrl);
+
   const onRadioCategoryChange = event => {
     const { value } = event.target;
     setSelectedCategoryValue(value);
@@ -383,6 +309,9 @@ const AddNoticeModal = ({ isModalOpen, setIsModalOpen }) => {
                       ) : null}
                     </ModalFieldLabel>
                     <ModalBtnWrap>
+                      {missedField && (
+                        <ErrorDesc>Please type required fields*</ErrorDesc>
+                      )}
                       <ModalBtn
                         type="button"
                         active
@@ -391,9 +320,6 @@ const AddNoticeModal = ({ isModalOpen, setIsModalOpen }) => {
                         }}
                       >
                         Next
-                        {missedField && (
-                          <ErrorDesc>Please type required fields *</ErrorDesc>
-                        )}
                       </ModalBtn>
 
                       <ModalBtn type="button" onClick={() => handleCancel()}>
@@ -407,6 +333,7 @@ const AddNoticeModal = ({ isModalOpen, setIsModalOpen }) => {
                       <span>The sex</span>
                     </ModalFieldLabel>
                     <ModalRadioGroup
+                      sx={{ marginBottom: '20px' }}
                       id="radiogroup"
                       onChange={e => {
                         handleInputChange(e, setFieldValue);
@@ -528,16 +455,18 @@ const AddNoticeModal = ({ isModalOpen, setIsModalOpen }) => {
                     </ModalFieldLabel>
                     <ModalBtnWrap>
                       {missedFielSecondStep && (
-                        <ErrorDesc>Please type required fields *</ErrorDesc>
+                        <ErrorDesc>Please type required* fields</ErrorDesc>
                       )}
+
+                      <ModalBtn type="submit" active>
+                        Done
+                      </ModalBtn>
+
                       <ModalBtn
                         type="button"
                         onClick={() => setFirstPage(true)}
                       >
                         Back
-                      </ModalBtn>
-                      <ModalBtn type="submit" active>
-                        Done
                       </ModalBtn>
                     </ModalBtnWrap>
                   </>
