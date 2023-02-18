0de412a71262df504ea4a4ded53c0094ef4c6af1
stgran66 <stanislaw.grygoriew@gmail.com>
2023-02-18 18:00:39 +0100

    fixed wrong icon display on page reload

--- src/components/Header/ThemeToggleButton/ThemeToggleButton.js
+++ src/components/Header/ThemeToggleButton/ThemeToggleButton.js
@@ -2,18 +2,23 @@ import { useSelector, useDispatch } from 'react-redux';
 import selectTheme from 'redux/theme/selectors';
 import styles from './ThemeToggleButton.styled';
 import { changeTheme } from 'redux/theme/themeSlice';
+import { useState } from 'react';
 
 const { ToggleButton } = styles;
 
 const ThemeToggleButton = () => {
   const dispatch = useDispatch();
   const theme = useSelector(selectTheme);
+
+  const [isChecked, setIsChecked] = useState(theme === 'light' ? true : false);
+
   return (
     <ToggleButton>
       <input
         type="checkbox"
-        defaultChecked={theme === 'ligth' ? true : false}
+        defaultChecked={isChecked}
         onChange={() => {
+          setIsChecked(!isChecked);
           dispatch(changeTheme());
         }}
       />
